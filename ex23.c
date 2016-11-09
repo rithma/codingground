1bea323ce86932103e04c7d3b3f35ff8678f5276
rithma <rithers@gmail.com>
2016-11-09 08:17:14 -0800

    Update ex23.c

--- ex23.c
+++ ex23.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 extern int randomNumberGen(int seed); 
 
 int main(int argc, char* argv[])
 {
     int random_number;
-    int i;
+    int i; len;
     char filename[20];
     
-    for (i=0; i<1000; i++)
+    strcpy(filename, "file_");               //copies "file_" string into the character array called 'filename'
+    printf("filename = %s\n", filename)      //outputs (prints)%string stored in 'filename'
+    
+    for (i=0; i<10; i++)
     {
     random_number = randomNumberGen(i);
     printf("seed = %d, rand = %d\n", i, random_number);
+    
     }
     return 0;
 }
@@ -22,3 +27,4 @@ int randomNumberGen(int seed)
     number = seed * 1103515245 + 12345;
     return (unsigned int)(number/65536) % 32768;
 }
+/**********************************************************
