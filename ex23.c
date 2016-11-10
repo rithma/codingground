529e2c820da7435ad53fef3bc3e880101384c1f1
rithma <rithers@gmail.com>
2016-11-10 07:07:36 -0800

    Update ex23.c

--- ex23.c
+++ ex23.c
@@ -8,22 +8,29 @@ int main(int argc, char* argv[])
     int random_number;
     int i, len;
     char filename[20];
-    
-    strcpy(filename, "file_");               //copies "file_" string into the character array called 'filename'
+  /*  
+    strcpy(filename, "file_");                                 //copies "file_" string into the character array called 'filename'
     len = strlen(filename);
     printf("filename = %s length = %d\n", filename, len);      //outputs (prints)%string stored in 'filename'
-    filename[len] = '0';                     //overwrites /0 null character with an '0'
-    filename[len + 1] = '\0';                //appends \0/NULL at the end of the string
+    filename[len] = '0'+ 1;                                       //overwrites /0 null character with an '0'
+    filename[len + 1] = '\0';                                  //appends \0/NULL at the end of the string
     len = strlen(filename);
     printf("filename = %s length = %d\n", filename, len);
     
-    strcat(filename, ".txt");                //concats .txt at the end of our filename 
+    strcat(filename, ".txt");                                  //concats .txt at the end of our filename 
     printf("filename = %s length = %d\n", filename, strlen(filename));
-    
+    */
     for (i=0; i<10; i++)
     {
-    random_number = randomNumberGen(i);
-    printf("seed = %d, rand = %d\n", i, random_number);
+        random_number = randomNumberGen(i);
+        printf("seed = %d, rand = %d\n", i, random_number);
+        
+        strcpy(filename, "file_");                                 //copies "file_" string into the character array called 'filename'
+        len = strlen(filename);
+        filename[len] = '0'+ i;                                       //overwrites /0 null character with an '0'
+        filename[len + 1] = '\0';                                  //appends \0/NULL at the end of the string
+        strcat(filename, ".txt");                                  //concats .txt at the end of our filename 
+        printf("filename = %s\n",filename);
     
     }
     return 0;
